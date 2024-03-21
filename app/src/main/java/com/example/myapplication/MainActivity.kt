ec547339c0a4827fbe4e0c82940fdc8dfbd8fd47
student <zhuhlin05@gmail.com>
2024-03-21 11:38:31 +0300

    asd

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = Adapter()
+
+        binding.button.setOnClickListener{
+            val text = binding.editTextText.text
+            if(text.toString().isNotEmpty()){
+                adapter.addValue(text.toString())
+            }
+        text.clear()
+        }
     }
 }
